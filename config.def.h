e4314590775f851c91a0a0a28f4f19aa61a0893d
yosemu <2197651308@qq.com>
2022-09-03 16:21:24 +0800

    changed the bord color when the window is inactive

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -18,7 +18,7 @@ static const char *fonts[]          = { "JetBrains Mono:pixelsize=25:antialias=t
 static const char dmenufont[]       = "monospace:size=10";
 static const char dmenuheight[]       = "100";
 static const char col_gray1[]       = "#121212";//状态栏底色
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#121212";
 static const char col_gray3[]       = "#bbbbbb";//没有被选中的title字体颜色
 static const char col_gray4[]       = "#121212";//被选中的tile字体颜色
 static const char col_cyan[]        = "#cccccc";//title底色
